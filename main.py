0572f8025d2563b2584e43b261a607f29aa82c3e
Andrii Sukhenko <andreysuha2@gmail.com>
2023-08-14 23:19:59 +0300

    fix

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 from normalize import normalize
 from sys import argv
 from pathlib import Path
+from config import OTHER_FOLDER, FOLDERS_DATA
 import os
 
-DEFAULT_PATH = Path(argv[1] if len(argv) >= 2 else './')
+if len(argv) < 2:
+    print('Param folder is required!')
 
-FOLDERS_DATA = {
-        "archives": ('ZIP', 'GZ', 'TAR'),
-        "video": ('AVI', 'MP4', 'MOV', 'MKV'),
-        "audio": ('MP3', 'OGG', 'WAV', 'AMR'),
-        "documents": ('DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX', 'CSV', 'XLS'),
-        "images": ('JPEG', 'PNG', 'JPG', 'SVG')
-    }
+DEFAULT_PATH = Path(argv[1])
 
 def create_directories(path):
     for name in FOLDERS_DATA:
@@ -21,7 +17,7 @@ def create_directories(path):
             os.mkdir(folder_path)
 
 def handle_file(path): 
-    parent_folder_name = None
+    parent_folder_name = OTHER_FOLDER
     file = path.name.split('.')
     file_ext = file.pop()
     file_name = '.'.join(file)
@@ -32,7 +28,7 @@ def handle_file(path):
 
     if parent_folder_name:
         file_name = f"{normalize(file_name)}{path.suffix}"
-        new_path = f"{path.parent}/{parent_folder_name}/{file_name}"
+        new_path = f"{DEFAULT_PATH}/{parent_folder_name}/{file_name}"
         os.rename(path, new_path)
         print(path, '-->', new_path)
 
@@ -42,11 +38,16 @@ def arrange(path):
     elif path.is_file():
         print(f"'{path.name}' is not a folder!")
     else:
-        create_directories(path)
         for i in path.iterdir():
             if i.is_dir() and i.name not in FOLDERS_DATA:
                 arrange(i)
+                try:
+                    os.rmdir(i)
+                except OSError as error:
+                    print(error)
+                    print(f"Path {i} can't be removed")
             elif i.is_file():
                 handle_file(i)
 
+create_directories(DEFAULT_PATH)
 arrange(DEFAULT_PATH)
